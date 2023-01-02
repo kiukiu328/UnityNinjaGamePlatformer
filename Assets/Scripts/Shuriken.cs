dddef839ddf34c3c0a5572c5ca4ec4efd2c38612
kiukiu328 <ctk328@yahoo.com.hk>
2023-01-02 20:18:12 +0800

    comment

--- Assets/Scripts/Shuriken.cs
+++ Assets/Scripts/Shuriken.cs
@@ -10,14 +10,13 @@ public class Shuriken : MonoBehaviour
         ShurikenObjs[0].transform.Rotate(0, 0, _speed * Time.deltaTime * 100);
         ShurikenObjs[1].transform.Rotate(0, 0, -_speed * Time.deltaTime * 100);
         ShurikenObjs[2].transform.Rotate(0, 0, _speed * Time.deltaTime * 100);
-        // float step = _movingSpeed * Time.deltaTime;
 
-        // // move sprite towards the target location
+
+        // move sprite towards the target location
         transform.position = new Vector2(
             transform.position.x,
             transform.position.y - Time.deltaTime * _speed
         );
-        // GetComponent<Rigidbody2D>().velocity = Vector2.down * _speed;
         if (transform.position.y < -6)
             transform.position = new Vector2(transform.position.x, transform.position.y * -1);
     }
